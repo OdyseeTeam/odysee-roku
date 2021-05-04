a7203884d4dfae6468d696b8f254e914783e6185
s9260 <s9260@halitesoftware.com>
2021-05-03 20:01:07 -0400

    asdf

--- MVP-Static-Trending-Generator/StaticGenerator.py
+++ MVP-Static-Trending-Generator/StaticGenerator.py
@@ -126,11 +126,11 @@ for key in masterfeed:
             if validvideo:
                 try: 
                     channelname = item['signing_channel']['value']['title']
-                    channelid = item['signing_channel']['address']
+                    channelid = item['signing_channel']['claim_id']
                 except:
                     try:
                         channelname = item['signing_channel']['value']['channel_id']
-                        channelid = item['signing_channel']['address']
+                        channelid = item['signing_channel']['claim_id']
                     except:
                         channelname = "Anonymous"
                         channelid = ""
