bf826716fe3813b895f289b452862e80c9c17a34
s9260 <s9260@halitesoftware.com>
2021-05-07 06:03:00 -0400

    crush further: remove unused double variable

--- MVP-Static-Trending-Generator/StaticGenerator.py
+++ MVP-Static-Trending-Generator/StaticGenerator.py
@@ -150,7 +150,7 @@ for key in masterfeed:
                     thumbnail = "pkg:\\images\\odyseeoops.png"
                 #https://api.lbry.com/file/view_count?auth_token=TOKEN&claim_id=CID
                 viewcount = numerize.numerize(json.loads(requests.get("https://api.lbry.com/file/view_count?auth_token="+authtoken+"&claim_id="+item['claim_id']).text)["data"][0],2)
-                entries.append([title, channelname, description, time.strftime('%a %b %e, %Y', time.localtime(item['timestamp'])), item['claim_id'], thumburl, "https://cdn.lbryplayer.xyz/api/v3/streams/free/"+item['normalized_name']+"/"+item['claim_id']+"/"+item['value']['source']['hash'][:6],"https://cdn.lbryplayer.xyz/api/v3/streams/free/"+item['normalized_name']+"/"+item['claim_id']+"/"+item['value']['source']['sd_hash'][:6], viewcount, channelid])
+                entries.append([title, channelname, description, time.strftime('%a %b %e, %Y', time.localtime(item['timestamp'])), item['claim_id'], thumburl, "https://cdn.lbryplayer.xyz/api/v3/streams/free/"+item['normalized_name']+"/"+item['claim_id']+"/"+item['value']['source']['hash'][:6],"u", viewcount, channelid])
                 numkeyentries+=1
         #standard: name, desc, pubdate, id, thumb, url
         #Temporary Redirect Fix: Vanwanet doesn't redirect properly.
